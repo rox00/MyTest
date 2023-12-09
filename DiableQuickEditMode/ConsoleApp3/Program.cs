25d152fc6abbefbe91d04c92780b852f9c208175
86131 <726372432@qq.com>
2023-12-09 14:32:34 +0800

    waiting for all thread to complete and return multithread json result

--- DiableQuickEditMode/ConsoleApp3/Program.cs
+++ DiableQuickEditMode/ConsoleApp3/Program.cs
@@ -1,5 +1,9 @@
 ﻿using ConsoleApp3.Common;
+using log4net.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -7,6 +11,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 
 namespace ConsoleApp3
 {
@@ -33,6 +38,99 @@ namespace ConsoleApp3
 
         static void Main(string[] args)
         {
+            Task t = new Task(() =>
+            {
+                Console.WriteLine("任务开始工作……");
+                Thread.Sleep(5000);  //模拟工作过程
+            });
+            t.Start();
+            t.ContinueWith(task =>
+            {
+                Console.WriteLine("任务完成，完成时候的状态为：");
+                Console.WriteLine("IsCanceled={0}\tIsCompleted={1}\tIsFaulted={2}",
+                                  task.IsCanceled, task.IsCompleted, task.IsFaulted);
+
+            });
+            /***********************************************************************************/
+            var tasks = new List<Task>();
+            ConcurrentDictionary<int,string> retDictionary = new ConcurrentDictionary<int,string>();    
+            // Define a delegate that prints and returns the system tick count
+            Action<object> action = (object obj) =>
+            {
+                string retStr = "";
+                int i = (int)obj;
+                try
+                {
+                    // Make each thread sleep a different time in order to return a different tick count
+                    Thread.Sleep(i * 100);
+
+                    // The tasks that receive an argument between 2 and 5 throw exceptions
+                    if (2 <= i && i <= 5)
+                    {
+                        throw new InvalidOperationException("SIMULATED EXCEPTION");
+                    }
+
+                    int tickCount = Environment.TickCount;
+                    Console.WriteLine("Task={0}, i={1}, TickCount={2}, Thread={3}", Task.CurrentId, i, tickCount, Thread.CurrentThread.ManagedThreadId);
+                    retStr = "success";
+                }
+                catch (Exception ex)
+                {
+                    retStr = ex.Message;
+                }
+
+                retDictionary[i] = retStr;
+            };
+
+            // Construct started tasks
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(action, i));
+            }
+
+            try
+            {
+                // Wait for all the tasks to finish.
+                Task.WaitAll(tasks.ToArray());
+                string jsonstr = JsonConvert.SerializeObject(retDictionary);
+
+                Dictionary<string,object> dic = new Dictionary<string,object>();
+                dic["statuscode"] = ErrorCode.GenericFailure;
+                dic["message"] = retDictionary;
+                string jsonstr1 = JsonConvert.SerializeObject(dic);
+
+                //{
+                //    "statuscode": 1,
+                //    "message": {
+                //        "0": "success",
+                //        "1": "success",
+                //        "2": "SIMULATED EXCEPTION",
+                //        "3": "SIMULATED EXCEPTION",
+                //        "4": "SIMULATED EXCEPTION",
+                //        "5": "SIMULATED EXCEPTION",
+                //        "6": "success",
+                //        "7": "success",
+                //        "8": "success",
+                //        "9": "success"
+                //    }
+                //}
+
+                // We should never get to this point
+                Console.WriteLine("WaitAll() has not thrown exceptions. THIS WAS NOT EXPECTED.");
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("\nThe following exceptions have been thrown by WaitAll(): (THIS WAS EXPECTED)");
+                for (int j = 0; j < e.InnerExceptions.Count; j++)
+                {
+                    Console.WriteLine("\n-------------------------------------------------\n{0}", e.InnerExceptions[j].ToString());
+                }
+            }
+
+
+
+
+
             DisbleQuickEditMode();
             string name = typeof(Program).Assembly.GetName().Name;
             CRegistryConfig registryCfg = new CRegistryConfig();
